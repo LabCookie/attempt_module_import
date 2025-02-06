09a7aaef9267b640139a68679de08bf53ae1e5e5
LabCookie <golf.le.fleur26@outlook.com>
2025-02-06 07:40:19 -0600

    dont_ask global variable

--- try_to_import.py
+++ try_to_import.py
@@ -1,9 +1,11 @@
 # This is a custom module that tries to import a module but if failed, the user has to choose to either install with pip or
 # exit the program alltogether. It will request you to restart the program after the installation of such module.
+dont_ask = False
 
 __all__ = [
     "attempt_import",
-    "attempt_from_import"
+    "attempt_from_import",
+	"dont_ask"
 ]
 
 import importlib
@@ -19,15 +21,18 @@ def clr():
     else:
         system("clear")
 def handle_importerror(module,e):
-    clr()
-    choice = ""
-    while not choice.upper() in ["Y","N"]: # Perform a loop until the user's choice (but capitalized) is either Y or N
-        choice = input(f"{e} Would you like to install it using pip? \nY/N: ")
-    if choice.upper() == "Y":
-        install_module(module)
-        input("Thank you. Press the enter key to continue")
-    else:
-        exit()
+	if not dont_ask:
+	    clr()
+	    choice = ""
+	    while not choice.upper() in ["Y","N"]: # Perform a loop until the user's choice (but capitalized) is either Y or N
+	        choice = input(f"{e} Would you like to install it using pip? \nY/N: ")
+	    if choice.upper() == "Y":
+	        install_module(module)
+	        input("Thank you. Press the enter key to continue")
+	    else:
+	        exit()
+	else:
+		install_module(module)
 def install_module(module):
     # Calls pip to install a module
     check_call([executable,"-m","pip","install",module])
